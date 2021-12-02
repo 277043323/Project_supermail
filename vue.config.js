9895bca76d55b8f3f4acbb51593261e23fb9f29d
guohongxia2 <277043323@qq.com>
2021-12-02 10:08:34 +0800

    更新项目配置

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    configureWebpack:{
+        resolve:{
+            alias:{
+                'asserts':'@/asserts',
+                'common':'@/common',
+                'components':'@/components',
+                'network':'@/network',
+                'views':'@/views',
+
+            }
+        }
+    }
+}
